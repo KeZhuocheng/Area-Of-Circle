1fe55952bccce4c3bac249524b00f6d075fc035d
KeZhuocheng <106873405+KeZhuocheng@users.noreply.github.com>
2022-07-04 19:41:27 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double r,a,h;
+    printf("Give the value of r:");
+    scanf("%lf",&r);
+    printf("GIve the value of h:");
+    scanf("%lf",&h);
+    a=2*3.1415*r*r+2*3.1415*h*r;
+
+    printf("The area is :%lf\n",a);
+    return 0;
+}
