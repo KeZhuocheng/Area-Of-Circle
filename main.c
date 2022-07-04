1aefc41b5074480153b38a57e51f19f2136413dc
KeZhuocheng <106873405+KeZhuocheng@users.noreply.github.com>
2022-07-04 19:43:08 +0200

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main()
     double r,a,h;
     printf("Give the value of r:");
     scanf("%lf",&r);
-    printf("GIve the value of h:");
+    printf("Give the value of h:");
     scanf("%lf",&h);
     a=2*3.1415*r*r+2*3.1415*h*r;
 
